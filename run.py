cc15da0b94b2f0f1f70e61b0dfd932fb7c1d6d4f
Rustap Chaudhary <elricgodz9@gmail.com>
2025-07-11 02:38:31 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,37 @@
-import os,time
-RED = "\033[91m"    # Bright Red
-WHITE = "\033[97m"  # Bright White
-bit = os.uname().machine
-os.system('chmod 777 *')
-if '64' in bit:
-  print(f' {RED}>> {WHITE}64 BIT DETECTED')
-  time.sleep(1)
-  print(f' {RED}>> {WHITE}CHECKING UPDATE')
-  time.sleep(2)
-  os.system('git checkout -- . && git pull')
-  print(f' {RED}>> {WHITE}UPDATE IS CHECKED')
-  time.sleep(2)
-  import ruth64
-else:
-  os.system('clear')
-  print(' {RED}>> {WHITE}32 BIT NOT SUPPORTED')
+#-----> MODULES IMPORT
+
+import platform
+import os
+import requests
+import time
+
+#-----> GLOBAL VALUES
+
+global arc
+
+#-----> CHECKING FOR UPDATES
+
+print(f' •\x1b[38;5;196m ->\x1b[37m CHECKING FOR UPDATES ')
+os.system('git pull --quiet')
+
+#-----> MAIN DEF
+
+def ruth():
+    global arc
+    architecture = platform.architecture()
+    if architecture[0] == '32bit':
+        arc = "32BIT"
+        print(f' •\x1b[38;5;196m ->\x1b[37m 32BIT DETECTED');time.sleep(1)
+        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING FILE TOOL ');time.sleep(1)
+    elif architecture[0] == '64bit':
+        arc = "64BIT"
+        print(f' •\x1b[38;5;196m ->\x1b[37m 64BIT DETECTED');time.sleep(1)
+        print(f' •\x1b[38;5;196m ->\x1b[37m STARTING FILE TOOL ');time.sleep(1)
+        import ruth64
+    else:
+        exit(f' •\x1b[38;5;196m ->\x1b[37m YOU CANT USE THIS TOOL ');time.sleep(1)
+
+#-----> SYSTEM CONTROL
+
+if __name__ == "__main__":
+    ruth()
