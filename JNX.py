ed3b31cdacd7468c6736b42f4930f68043c4128f
Rustap Chaudhary <elricgodz9@gmail.com>
2025-07-10 20:08:09 +0545

    Update JNX.py

--- JNX.py
+++ JNX.py
@@ -5,7 +5,7 @@ os.system('chmod 777 *')
 if '64' in bit:
   print(' YOUR SYSTEM IS 64 BIT SO SUCCESS RUN')
   time.sleep(2)
-  from jnx64 import key
+  import jnx64
 else:
   os.system('clear')
   print(' YOUR SYSTEM IS 32 BIT NOT SUPPORTED')
