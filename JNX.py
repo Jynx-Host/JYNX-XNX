ab90b20e356bee6fceaae7c0903b592895cc39ad
Rustap Chaudhary <elricgodz9@gmail.com>
2025-07-10 20:36:47 +0545

    Update JNX.py

--- JNX.py
+++ JNX.py
@@ -1,9 +1,14 @@
+
 import os,time
+RED = "\033[91m"    # Bright Red
+WHITE = "\033[97m"  # Bright White
 bit = os.uname().machine
-os.system('git checkout -- . && git pull')
 os.system('chmod 777 *')
 if '64' in bit:
-  print(' YOUR SYSTEM IS 64 BIT SO SUCCESS RUN')
+  print(f' {RED}>> {WHITE}64 BIT DETECTED')
+  print(f' {RED}>> {WHITE}CHECKING UPDATE')
+  os.system('git checkout -- . && git pull')
+  print(f' UPDATE IS CHECKED')
   time.sleep(2)
   import jnx64
 else:
